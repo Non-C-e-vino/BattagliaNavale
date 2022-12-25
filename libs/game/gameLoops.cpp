0603e78a5e9297f65b44a6335b48fc2d186ba9ab
SadanoRapa <gabriele.callegaro@gmail.com>
2022-12-25 04:40:52 +0100

    aggiunti commenti

--- libs/game/gameLoops.cpp
+++ libs/game/gameLoops.cpp
@@ -1,21 +1,21 @@
 #include "gameLoops.h"
 
-void gameLoops::init_loop(const std::unique_ptr<Player> &player1, const std::unique_ptr<Player> &player2, GameHandler& gm){
+void gameLoops::init_loop(const std::unique_ptr<Player> &player1, const std::unique_ptr<Player> &player2, GameHandler& gh){
     player1->get_input();
 }
 
-void gameLoops::main_loop(const std::unique_ptr<Player> &player1, const std::unique_ptr<Player> &player2, GameHandler& gm){
+void gameLoops::main_loop(const std::unique_ptr<Player> &player1, const std::unique_ptr<Player> &player2, GameHandler& gh){
     player1->get_input();
 }
 
 void gameLoops::game_loop(bool pc){
-    GameHandler gm{};
+    GameHandler gh{};
     std::unique_ptr<Player> player1;
     std::unique_ptr<Player> player2;
     if(pc) player1 = std::make_unique<HumanPlayer>();
     else player1 = std::make_unique<Bot>();
     player2 = std::make_unique<Bot>();
-    init_loop(player1, player2, gm);
-    main_loop(player1, player2, gm);
+    init_loop(player1, player2, gh);
+    main_loop(player1, player2, gh);
 }
 
